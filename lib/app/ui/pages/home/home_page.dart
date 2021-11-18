d99ab4cc67c38dd8a6918a1815f7f8ab11a9cb8f
Nicol Gutirierrez <jhoanarodbar@gmail.com>
2021-11-17 16:08:05 -0800

    Avance hasta video 29 funciona bien.

--- lib/app/ui/pages/home/home_page.dart
+++ lib/app/ui/pages/home/home_page.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:movil/app/ui/pages/home/home_controller.dart';
+import 'package:movil/app/ui/pages/home/widgets/google_map.dart';
 import 'package:provider/provider.dart';
 
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-
-
 
 
   @override
@@ -22,7 +19,26 @@ class HomePage extends StatelessWidget {
         return controller;
       },
       child: Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+         Builder(builder: (context) =>  IconButton(
+           onPressed: (){
+          final controller = context.read<HomeController>();
+          controller.newPolyline();
+          }, 
+          icon: const Icon(Icons.add),
+          ),
+          ),
+          Builder(builder: (context) =>  IconButton(
+           onPressed: (){
+          final controller = context.read<HomeController>();
+          controller.newPolygon();
+          }, 
+          icon: const Icon(Icons.map),
+          ),
+          )
+        ],
+      ),
       body: Selector<HomeController, bool>(
         selector: (_,controller) => controller.loading,
         builder: (context, loading, loadingWidget){
@@ -31,48 +47,7 @@ class HomePage extends StatelessWidget {
           return loadingWidget!;
         }
         
-        return Consumer<HomeController>(
-        builder:(_, controller, gpsMessageWidget) {
-
-          if(!controller.gpsEnabled){
-            return gpsMessageWidget!;
-          }
-        
-        final initialPosition = LatLng(
-          controller.initialPosition!.latitude, 
-          controller.initialPosition!.longitude);
-
-          return GoogleMap(
-              markers: controller.markers,
-              onMapCreated: controller.onMapCreated,
-              initialCameraPosition: CameraPosition(target: initialPosition, zoom: 14),
-              myLocationButtonEnabled: true,
-              myLocationEnabled: true,
-              compassEnabled: false,
-              onTap: controller.onTap,
-              );
-           },
-
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text("Para utilizar nuestra app necesita habilidar el GPS",
-                  textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: (){
-                     final controller = context.read<HomeController>();
-                    controller.turnOnGPS();
-                    }, 
-                    child: const  Text("Turn on GPS"),
-                    ),               
-                ],
-              ),
-            ),
-
-              );
+        return  const  MapView();
         },
              child: const Center(
             child: CircularProgressIndicator(),
